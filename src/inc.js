389f6b6765a4b23b6d7d725482db6e9fba196627
Alex Gusev <alex@flancer64.com>
2017-03-29 16:37:37 +0300

    MOBI-669 - Move MOBI tests to standalone projects

--- src/inc.js
+++ src/inc.js
@@ -1,15 +1,2 @@
 "use strict"
 /** Include doesn't require 'casper.test.done()' call inside. Executes before each test in the suite (after 'pre'). */
-
-if (typeof require.globals === 'object') {
-    console.log("SlimerJS is detected.");
-    /* include MOBI staff if not included yet and place 'casper' object inside */
-    var mobi = mobi || require("./mobi/index")
-    mobi.casper = mobi.casper || casper
-    require.globals.mobi = mobi
-} else {
-    console.log("PhantomJS is detected.");
-    /* include MOBI staff if not included yet and place 'casper' object inside */
-    var mobi = mobi || require("./mobi/index")
-    mobi.casper = mobi.casper || casper
-}
