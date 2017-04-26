91ef2fc0ffeae0856cddf4cd294bd464fee155a3
Alex Gusev <alex@flancer64.com>
2017-04-26 15:35:02 +0300

    MOBI-670 - Configuration JSON for integration tests

--- src/mobi/cfg/index.js
+++ src/mobi/cfg/index.js
@@ -4,7 +4,7 @@
  *
  * see 'docs/config.md'
  */
-var cfgGlobal = require("./global")
+var cfgGlobal = require("./global") // don't use 'global' name
 var cfgProject = require("../../../etc/cfg/project")
 var cfgLocal = require("../../../etc/cfg/local")
 
@@ -12,17 +12,12 @@ var cfgLocal = require("../../../etc/cfg/local")
  * 'mobi' object that is returned by 'global.getTestContext' is not defined yet. 'require' chain should be done
  * completely to get result. So, load npm lib explicitly.
  */
-var objMerge = require("../../../node_modules/deepmerge/index") // see https://www.npmjs.com/package/deepmerge
+var merge = require("../../../node_modules/deepmerge/index") // see https://www.npmjs.com/package/deepmerge
 
 /**
  * Merge base. project & local configurations into common one.
  */
-var merged = objMerge(cfgGlobal, cfgProject)
+var cfg = merge(cfgGlobal, cfgProject)
+cfg = merge(cfg, cfgLocal)
 
-console.log("CONFIG 00: " + JSON.stringify(merged))
-
-merged = objMerge(merged, cfgLocal)
-
-console.log("CONFIG 01: " + JSON.stringify(merged))
-
-module.exports = merged
+module.exports = cfg
