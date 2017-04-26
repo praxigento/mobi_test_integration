f4ab1664a174b37afb42f2fc25f4e00e4459a42c
Alex Gusev <alex@flancer64.com>
2017-04-26 15:09:37 +0300

    MOBI-670 - Configuration JSON for integration tests

--- src/mobi/cfg/index.js
+++ src/mobi/cfg/index.js
@@ -12,12 +12,17 @@ var cfgLocal = require("../../../etc/cfg/local")
  * 'mobi' object that is returned by 'global.getTestContext' is not defined yet. 'require' chain should be done
  * completely to get result. So, load npm lib explicitly.
  */
-var objMerge = require("../../../node_modules/merge-recursive/lib/index") // see https://github.com/UmbraEngineering/node-merge-recursive
+var objMerge = require("../../../node_modules/deepmerge/index") // see https://www.npmjs.com/package/deepmerge
 
 /**
  * Merge base. project & local configurations into common one.
  */
 var merged = objMerge(cfgGlobal, cfgProject)
+
+console.log("CONFIG 00: " + JSON.stringify(merged))
+
 merged = objMerge(merged, cfgLocal)
+
+console.log("CONFIG 01: " + JSON.stringify(merged))
 
 module.exports = merged
--- src/mobi/util/index.js
+++ src/mobi/util/index.js
@@ -2,8 +2,10 @@
 /**
  * MOBI common utils (test related utils are placed in 'mobi.test.util')
  */
+var objMerge = require("../../../node_modules/deepmerge/index") // see https://www.npmjs.com/package/deepmerge
+var objPath = require("../../../node_modules/object-path/index") // see https://github.com/mariocasciaro/object-path
 
 module.exports = {
-    objMerge: require("../../../node_modules/merge-recursive/lib/index"), // see https://github.com/UmbraEngineering/node-merge-recursive
-    objPath: require("../../../node_modules/object-path/index") // see https://github.com/mariocasciaro/object-path
+    objMerge: objMerge,
+    objPath: objPath
 }
