35bfa33658223da90ec78132b3c3b153560609d6
Alex Gusev <alex@flancer64.com>
2017-08-02 12:32:13 +0300

    MOBI-836 - Restore integration tests

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # Integration tests for MOBI projects
 
-### Installation
 
-  npm install
+
+## Installation
+
+
+### Application 
+
+```bash
+$ npm install
+```
+
+
+### Chrome
+
+Ubuntu:
+```bash
+# wget  https://dl-ssl.google.com/linux/linux_signing_key.pub
+# apt-key add linux_signing_key.pub
+# sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+# apt-get update
+# apt-get install google-chrome-stable
+```
 
 
 
--- scenario/000/000.js
+++ scenario/000/000.js
@@ -2,7 +2,7 @@
  * Just check MOBI components (Magento/Odoo web sites).
  */
 const mobi = require('../../src/mobi');
-
+const desc = 'MOBI components checking';
 /**
  * @param {mobi} mobi
  * @returns {Promise.<void>}
@@ -10,7 +10,16 @@ const mobi = require('../../src/mobi');
 async function main(mobi) {
     const chromy = mobi.chromy;
 
+    console.log('Start scenario: "%s".', desc);
+
+    console.log('Check Magento front.');
     await chromy.goto(mobi.cfg.url.mage.front.base);
+
+    console.log('Check Magento backend.');
+    await chromy.goto(mobi.cfg.url.mage.admin.base);
+    await chromy.wait(1000);
+
+    console.log('End scenario: "%s".', desc);
 
     if (!mobi.batch) {
         await chromy.close();
